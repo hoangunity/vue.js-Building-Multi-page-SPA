7d3c47f24f042953915173f00e62449e56198593
hoangunity <hoangdm.141196@gmail.com>
2021-01-21 14:35:30 +0700

    Redirecting & CatchAll Routes

--- src/main.js
+++ src/main.js
@@ -5,13 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    // { path: '/', component: TeamsList }, // alternative => alias ⬇
+    // { path: '/teams', component: TeamsList, alias: '/' },
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true }
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 });
