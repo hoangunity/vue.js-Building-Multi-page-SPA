5a87626899154995de8cf1ef289a64d0eeff90d2
hoangunity <hoangdm.141196@gmail.com>
2021-01-22 09:38:54 +0700

    Using Query Params

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -46,6 +46,7 @@ export default {
     // console.log(this.$route);
     // this.$route.path // /teams/t1
     this.loadTeamMembers(this.teamId);
+    console.log(this.$route.query, this.sort);
   },
   watch: {
     teamId(newId) {
